53c4bf0f14a7010534439ff73352ea8507335984
davibelini <dev.dado.belini@gmail.com>
2021-01-23 13:37:03 -0300

    fixed batch assignment syntax

--- playground/batch-fire-implementation/fire.bat
+++ playground/batch-fire-implementation/fire.bat
@@ -1,19 +1,18 @@
 @echo off
 
-set /a fire_pixels_array[0]=0
-set /a fire_width = 2
-set /a fire_height = 3
+set fire_pixels_array[0]=0
+set /a fire_width=2
+set /a fire_height=3
 
 :start
   call :create_datastructure
-  echo %fire_pixels_array[0]%
-  exit /B 0
+  echo %fire_pixels_array%
+exit /B 0
 
 :create_datastructure
   set /a number_of_pixels ="%fire_width% * %fire_height%"
-  for /l %%i in (0, 1, %number_of_pixels%) do set /a %fire_pixels_array[%number_of_pixels%]% = 0
-  echo hello
-  exit /B 0
+  for /l %%i in (0, 1, %number_of_pixels%) do (set %fire_pixels_array[%number_of_pixels%]%=0)
+exit /B 0
 
 :calc_fire_propag
 exit /B 0
