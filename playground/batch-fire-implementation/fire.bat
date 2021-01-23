21ef624a4862e48c34a664fbb8105c4857820b9b
davibelini <dev.dado.belini@gmail.com>
2021-01-23 14:30:48 -0300

    created intensity palette

--- playground/batch-fire-implementation/fire.bat
+++ playground/batch-fire-implementation/fire.bat
@@ -1,12 +1,28 @@
 @echo off
 
+set palette[0]=^.
+set palette[1]=^:
+set palette[2]=^-
+set palette[3]=^=
+set pallete[4]=^+
+set palette[5]=^*
+set palette[6]=^#
+set palette[7]=^%
+set palette[8]=^@
+
+::set pallete=. : - = + * # % @
+
+for %%g in (%palette%) do (
+  echo %%g
+)
+
 set fire_pixels_array[0]=0
-set /a fire_width=2
+set /a fire_width=5
 set /a fire_height=3
 
 :start
   call :create_datastructure
-  echo %fire_pixels_array%
+  ::echo %fire_pixels_array%
 exit /B 0
 
 :create_datastructure
