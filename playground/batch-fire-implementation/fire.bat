3cc24468418190c70de6202ec4d8b2c9d2c4abc9
davibelini <dev.dado.belini@gmail.com>
2021-01-23 13:33:13 -0300

    started batch script implementation

---
+++ playground/batch-fire-implementation/fire.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set /a fire_pixels_array[0]=0
+set /a fire_width = 2
+set /a fire_height = 3
+
+:start
+  call :create_datastructure
+  echo %fire_pixels_array[0]%
+  exit /B 0
+
+:create_datastructure
+  set /a number_of_pixels ="%fire_width% * %fire_height%"
+  for /l %%i in (0, 1, %number_of_pixels%) do set /a %fire_pixels_array[%number_of_pixels%]% = 0
+  echo hello
+  exit /B 0
+
+:calc_fire_propag
+exit /B 0
+
+:render_fire
+exit /B 0
+
+call :start
